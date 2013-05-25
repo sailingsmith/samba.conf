ca32f708f87b16d8ff674cca11816f862278628c
x_ig@qq.com <yy@cl234.(none)>
2013-05-25 10:40:38 +0800

    add mkdir.sh

---
+++ mkdirusers.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#FileName: addusers.sh
+
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+
+#if [ $UID -ne 0 ];then
+#   echo "current user is not root,current user is $USER"
+#   exit 0
+#fi
+
+
+userfile="userlist"
+users=`awk  '{print $1}' $userfile`
+
+
+
+for user in $users
+do
+    password=`grep $user $userfile | awk '{print $2}'`
+    userid=`echo "$user" | cut -b 3-5`
+    userid=2$userid
+    #userid=$((2000+$userid))
+    mkdir $user
+    chown $user $user
+    #chmod 700 $user
+    
+
+    #echo "$password\n$user"
+    echo "mkdir $user"
+done 
+
